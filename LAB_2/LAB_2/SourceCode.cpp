dbfe960298ca85ca514860ee2b85c391d10670c3
den4ik-j <den4ik.j@gmail.com>
2013-09-19 02:42:42 +0600

    some fix

--- LAB_2/LAB_2/SourceCode.cpp
+++ LAB_2/LAB_2/SourceCode.cpp
@@ -8,6 +8,7 @@ Matrix::Matrix(void)
 	Array=new double* [lengthArray];
 	for(int i=0; i<lengthArray;i++)
 		Array[i]=new double [hightArray];	
+	cout<<"Работа конструктора\n";
 }
 Matrix::Matrix(int length,int hight)
 {
@@ -21,14 +22,27 @@ Matrix::Matrix(int length,int hight)
 	Array=new double* [lengthArray];
 	for(int i=0; i<lengthArray;i++)
 		Array[i]=new double [hightArray];
+	cout<<"Работа конутрутора с параметрами\n";
 }
 Matrix::~Matrix(void)
 {
 	for(int i=0; i<lengthArray;i++)
 		delete [] Array[i];
 	delete [] Array;
+	cout<<"Работа деструтора\n";
 }
-
+Matrix::Matrix(const Matrix &ob)
+{
+	this->lengthArray=ob.lengthArray;
+	this->hightArray=ob.hightArray;
+	Array=new double* [this->lengthArray];
+	for(int i=0; i<this->lengthArray;i++)
+		Array[i]=new double [hightArray];
+	for(int i=0;i<this->lengthArray;i++)
+		for(int j=0;j<this->hightArray;j++)
+			this->Array[i][j]=ob.Array[i][j];
+	cout<<"Работа конструктора копий\n";
+}
 
 Matrix Matrix::operator+(Matrix& ob1)
 {
@@ -38,7 +52,7 @@ Matrix Matrix::operator+(Matrix& ob1)
 		exit(1);
 	}
 	Matrix temp;
-	for(int i;i<lengthArray;i++)
+	for(int i=0;i<lengthArray;i++)
 		for(int j=0;j<hightArray;j++)
 			temp.Array[i][j]=Array[i][j]+ob1.Array[i][j];
 	return temp;
@@ -46,7 +60,7 @@ Matrix Matrix::operator+(Matrix& ob1)
 Matrix Matrix::operator+(double var)
 {
 	Matrix temp;
-	for(int i;i<lengthArray;i++)
+	for(int i=0;i<lengthArray;i++)
 		for(int j=0;j<hightArray;j++)
 			temp.Array[i][j]=Array[i][j]+var;
 	return temp;
@@ -59,7 +73,7 @@ Matrix Matrix::operator-(Matrix& ob1)
 		exit(1);
 	}
 	Matrix temp;
-	for(int i;i<lengthArray;i++)
+	for(int i=0;i<lengthArray;i++)
 		for(int j=0;j<hightArray;j++)
 			temp.Array[i][j]=Array[i][j]-ob1.Array[i][j];
 	return temp;
@@ -67,16 +81,15 @@ Matrix Matrix::operator-(Matrix& ob1)
 Matrix Matrix::operator-(double var)
 {
 	Matrix temp;
-	for(int i;i<lengthArray;i++)
+	for(int i=0;i<lengthArray;i++)
 		for(int j=0;j<hightArray;j++)
 			temp.Array[i][j]=Array[i][j]-var;
 	return temp;
 }
-
 Matrix Matrix::operator*(Matrix& ob1)
 {
 	Matrix temp;
-	for(int i;i<lengthArray;i++)
+	for(int i=0;i<lengthArray;i++)
 		for(int j=0;j<hightArray;j++)
 			for(int r=0;r<lengthArray;r++)
 				temp.Array[i][j]+=Array[i][r]*ob1.Array[r][j];
@@ -85,16 +98,22 @@ Matrix Matrix::operator*(Matrix& ob1)
 Matrix Matrix::operator*(double var)
 {
 	Matrix temp;
-	for(int i;i<lengthArray;i++)
+	for(int i=0;i<lengthArray;i++)
 		for(int j=0;j<hightArray;j++)
 			temp.Array[i][j]*=var;
 	return temp;
 }
 Matrix& Matrix::operator=(Matrix& ob)
 {
-	this->sizeArray=ob.sizeArray;
-	for(int i=0;i<this->sizeArray;i++)
-		for(int j=0;j<this->sizeArray;j++)
+	if(this->lengthArray!=ob.lengthArray)
+	{
+		// Error: defferent size of matrix;
+		exit(1);
+	}
+	this->lengthArray=ob.lengthArray;
+	this->hightArray=ob.hightArray;
+	for(int i=0;i<this->lengthArray;i++)
+		for(int j=0;j<this->hightArray;j++)
 			this->Array[i][j]=ob.Array[i][j];
 	return *this;
 }
@@ -112,16 +131,20 @@ double& Matrix::operator[](int index)
 
 
 
-
+Matrix& testfun(Matrix &ob)
+{
+ return ob;
+}
 
 
 
 int main() 
 {
+	setlocale(LC_ALL, "Russian");
     Matrix a;
-    Matrix *b();
-    Matrix c(3,3);
-
+    //Matrix *b();
+    //Matrix c(3,3);
+	testfun(a);
    // for (int i = 0; i < 2; i++)
    //     for (int j = 0; j < 2; j++) 
 			//b[i][j] = i+j;
@@ -141,5 +164,6 @@ int main()
         << c << endl
         << b << endl;
 */
+	system("pause");
     return 0;
 }
